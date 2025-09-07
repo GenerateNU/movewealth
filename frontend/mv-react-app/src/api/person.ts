462e41e0584f7cef6939e47de883a948ebbe92b6
Adam Ma <adam.zym@gmail.com>
2025-09-07 15:58:22 -0400

    Test Lint

--- frontend/mv-react-app/src/api/person.ts
+++ frontend/mv-react-app/src/api/person.ts
@@ -6,4 +6,4 @@ const baseURL = 'http://localhost:8080/persons';
 // TODO: call the get person endpoint
 export async function getAllPersons(): Promise<Person[]> {
   return makeRequest<Person[]>(baseURL, 'GET');
-};
+}
