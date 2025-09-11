e35ef08f254ab9a40fa848d5ba952660e7119761
Adam Ma <adam.zym@gmail.com>
2025-09-10 20:01:14 -0400

    Undo

--- backend/app/api/endpoints/health.py
+++ backend/app/api/endpoints/health.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-# Test
 @router.get("/", status_code=200)
 async def health_check():
     return {"status": "ok", "message": "API is running"}
